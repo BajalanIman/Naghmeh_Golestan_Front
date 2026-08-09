5cb1d22be84d59ec1f22d90c490288feee755eb4
Iman <bajalaniman@gmail.com>
2026-08-09 14:38:43 +0200

    adding constant

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,2 @@
-//export const BASE_URL = "marcobackend-production.up.railway.app/";
-export const BASE_URL = "http://localhost:8800/api/";
+export const BASE_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:8800/api/";
